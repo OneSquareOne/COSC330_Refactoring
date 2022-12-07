4f31ba26fb48b3a86e60ed29c770d3c1bf019059
OneSquareOne <104479171+OneSquareOne@users.noreply.github.com>
2022-12-06 22:41:06 -0500

    Phase 1

---
+++ TestCase/src/JunitClass.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+public class JunitClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+@Test
+	public void JunitMethod() {
+		
+	}
+}
