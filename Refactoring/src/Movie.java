0381570b6920c0ed348df9b4e9f52cd22685bebd
OneSquareOne <johnls1986@gmail.com>
2022-12-07 10:11:16 -0500

    Phase 2-9

--- Refactoring/src/Movie.java
+++ Refactoring/src/Movie.java
@@ -24,5 +24,25 @@ public class Movie {
     public void setPriceCode(int priceCode) {
         this.priceCode = priceCode;
     }
+    
+    public double getCharge(int daysRented) {
+    	double rentalPrice = 0;
+    	switch (this.getPriceCode()) {
+		case Movie.REGULAR:
+			rentalPrice += 2;
+			if (daysRented > 2)
+				rentalPrice += (daysRented - 2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			rentalPrice += daysRented * 3;
+			break;
+		case Movie.CHILDREN:
+			rentalPrice += 1.5;
+			if (daysRented > 3)
+				rentalPrice += (daysRented - 3) * 1.5;
+			break;
+		}
+    	return rentalPrice;
+    }
 
 }
--- Refactoring/src/TestClass.java
+++ Refactoring/src/TestClass.java
@@ -15,37 +15,37 @@ public class TestClass {
     @Test
     public void basicChildrenRental() {
         customer.addRental(new Rental(THE_HULK, 2));
-        assertThat(customer.statement(), is("Rental record for fred\n\tThe Hulk\t1.5\nAmount owed is 1.5\nYou earned 1 frequent renter points"));
+        assertEquals(customer.statement(), "Rental record for fred\n\tThe Hulk\t1.5\nAmount owed is 1.5\nYou earned 1 frequent renter points");
     }
 
     @Test
     public void shouldDiscountChildrensRentals() {
         customer.addRental(new Rental(THE_HULK, 4));
-        assertThat(customer.statement(), is("Rental record for fred\n\tThe Hulk\t3.0\nAmount owed is 3.0\nYou earned 1 frequent renter points"));
+        assertEquals(customer.statement(), "Rental record for fred\n\tThe Hulk\t3.0\nAmount owed is 3.0\nYou earned 1 frequent renter points");
     }
 
     @Test
     public void basicNewReleaseRental() {
         customer.addRental(new Rental(IRON_MAN, 1));
-        assertThat(customer.statement(), is("Rental record for fred\n\tIron Man 4\t3.0\nAmount owed is 3.0\nYou earned 1 frequent renter points"));
+        assertEquals(customer.statement(), "Rental record for fred\n\tIron Man 4\t3.0\nAmount owed is 3.0\nYou earned 1 frequent renter points");
     }
 
     @Test
     public void shouldNotDiscountNewReleaseRentalsButBonusFrequentRenterPoints() {
         customer.addRental(new Rental(IRON_MAN, 4));
-        assertThat(customer.statement(), is("Rental record for fred\n\tIron Man 4\t12.0\nAmount owed is 12.0\nYou earned 2 frequent renter points"));
+        assertEquals(customer.statement(), "Rental record for fred\n\tIron Man 4\t12.0\nAmount owed is 12.0\nYou earned 2 frequent renter points");
     }
 
     @Test
       public void basicRegularRental() {
           customer.addRental(new Rental(SPIDER_MAN, 2));
-          assertThat(customer.statement(), is("Rental record for fred\n\tSpiderman\t2.0\nAmount owed is 2.0\nYou earned 1 frequent renter points"));
+          assertEquals(customer.statement(), "Rental record for fred\n\tSpiderman\t2.0\nAmount owed is 2.0\nYou earned 1 frequent renter points");
       }
 
       @Test
       public void shouldDiscountRegularRental() {
           customer.addRental(new Rental(SPIDER_MAN, 4));
-          assertThat(customer.statement(), is("Rental record for fred\n\tSpiderman\t5.0\nAmount owed is 5.0\nYou earned 1 frequent renter points"));
+          assertEquals(customer.statement(), "Rental record for fred\n\tSpiderman\t5.0\nAmount owed is 5.0\nYou earned 1 frequent renter points");
       }
 
     @Test
@@ -53,7 +53,7 @@ public class TestClass {
         customer.addRental(new Rental(THE_HULK, 2));
         customer.addRental(new Rental(SPIDER_MAN, 1));
         customer.addRental(new Rental(IRON_MAN, 3));
-        assertThat(customer.statement(), is("Rental record for fred\n\tThe Hulk\t1.5\n\tSpiderman\t2.0\n\tIron Man 4\t9.0\nAmount owed is 12.5\nYou earned 4 frequent renter points"));
+        assertEquals(customer.statement(), "Rental record for fred\n\tThe Hulk\t1.5\n\tSpiderman\t2.0\n\tIron Man 4\t9.0\nAmount owed is 12.5\nYou earned 4 frequent renter points");
 
     }
 
