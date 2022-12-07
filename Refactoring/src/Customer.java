a163ea24d8c327004507c7f7b4658a13c104f6dd
OneSquareOne <104479171+OneSquareOne@users.noreply.github.com>
2022-12-06 23:09:29 -0500

    Phase 2- 3

--- Refactoring/src/Customer.java
+++ Refactoring/src/Customer.java
@@ -5,67 +5,54 @@ import org.junit.Test;
 
 public class Customer {
 
-    private String name;
-    private List<Rental> rentals = new ArrayList<Rental>();
+	private String name;
+	private List<Rental> rentals = new ArrayList<Rental>();
 
-    public Customer(String name) {
-        this.name = name;
-    }
+	public Customer(String name) {
+		this.name = name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void addRental(Rental rental) {
-        rentals.add(rental);
-    }
+	public void addRental(Rental rental) {
+		rentals.add(rental);
+	}
 
-    @Test
-    public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
+	@Test
+	public String statement() {
+		double totalAmount = 0;
+		int frequentRenterPoints = 0;
 
-        String result = "Rental record for " + getName() + "\n";
-        for (Rental rental : rentals) {
-            double amount = 0;
-           
-            amount = amountFor(rental);
+		String result = "Rental record for " + getName() + "\n";
+		for (Rental rental : rentals) {
+			double amount = 0;
 
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1)
-                frequentRenterPoints++;
+			
+			amount = amountFor(rental);
 
-            // show figures for this rental
-            result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(amount) + "\n";
+			// add frequent renter points
+			frequentRenterPoints++;
+			// add bonus for a two day new release rental
+			if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1)
+				frequentRenterPoints++;
 
-            totalAmount += amount;
-        }
+			// show figures for this rental
+			result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(amount) + "\n";
 
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+			totalAmount += amount;
+		}
 
-        return result;
-    }
-    
-    public double amountFor(Rental rental) {
-    	double rentalPrice = 0;
-    	switch (rental.getMovie().getPriceCode()) {
-        case Movie.REGULAR:
-        	rentalPrice += 2;
-            if (rental.getDaysRented() > 2)
-            	rentalPrice += (rental.getDaysRented() - 2) * 1.5;
-            break;
-        case Movie.NEW_RELEASE:
-        	rentalPrice += rental.getDaysRented() * 3;
-            break;
-        case Movie.CHILDREN:
-        	rentalPrice += 1.5;
-            if (rental.getDaysRented() > 3)
-            	rentalPrice += (rental.getDaysRented() - 3) * 1.5;
-            break;
-    }
-    	return rentalPrice;
-    }
+		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+
+		return result;
+	}
+
+	//fixed in 2-2
+	public double amountFor(Rental rental)  {
+	
+		return rental.getCharge();
+	}
 }
--- Refactoring/src/Rental.java
+++ Refactoring/src/Rental.java
@@ -18,5 +18,25 @@ public class Rental {
     public int getDaysRented() {
         return daysRented;
     }
+    
+    public double getCharge() {
+    	double rentalPrice = 0;
+		switch (this.getMovie().getPriceCode()) {
+		case Movie.REGULAR:
+			rentalPrice += 2;
+			if (this.getDaysRented() > 2)
+				rentalPrice += (this.getDaysRented() - 2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			rentalPrice += this.getDaysRented() * 3;
+			break;
+		case Movie.CHILDREN:
+			rentalPrice += 1.5;
+			if (this.getDaysRented() > 3)
+				rentalPrice += (this.getDaysRented() - 3) * 1.5;
+			break;
+		}
+		return rentalPrice;
+    }
 
 }
